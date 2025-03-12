8e0083a86278200c0705fa8d421f4c396f8a304d
WangYan <topshihun@foxmail.com>
2025-03-12 17:28:12 +0800

    Add: a kaymap, two lsp for lspconfig
    kaymap: Ctrl+s to save the file
    c/c++ lsp: clangd
    verilog/sysverilog: svls

--- lua/config/kaymap.lua
+++ lua/config/kaymap.lua
@@ -1,14 +1,15 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opt = {noremap = true, silent = true }
 
 map("n", "<C-h>", "<C-w>h", opt)
 map("n", "<C-j>", "<C-w>j", opt)
 map("n", "<C-k>", "<C-w>k", opt)
 map("n", "<C-l>", "<C-w>l", opt)
-map("n", "<C-v>", "<C-w>v", opt)
-map("n", "<C-s>", "<C-w>s", opt)
+
+-- fast sava
+map({"n", "i"}, "<C-s>", "<esc><Cmd>w<CR>", {silent = true})
 
 -- map("n", "<Tab>", ":NvimTreeToggle", opt)
--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -4,9 +4,16 @@ if not nlsp_status then
   return
 end
 
+-- typst
 require("lspconfig")["tinymist"].setup {
   settings = {
     formatterMode = "typstyle",
     semanticTokens = "disable"
   }
 }
+
+-- c/c++
+require("lspconfig")["clangd"].setup {}
+
+-- verilog/systemverilog
+require("lspconfig")["svls"].setup {}
